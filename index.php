26db30e36586592466ff58030a6d10c4b118e228
netgomail <netgo@mail.ru>
2017-08-07 23:14:02 +0500

    Commit new

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Ямал");
 ?>
-<h1>Commit</h1>
+<h1>Commit changes</h1>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
